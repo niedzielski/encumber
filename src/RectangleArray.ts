7c2da0ed534450982ffc2090f6b2809bef283de0
Stephen Niedzielski <stephen@niedzielski.com>
2017-05-01 00:37:36 -0600

    New: add RectangleArray

---
+++ src/RectangleArray.ts
@@ -0,0 +1,87 @@
+import Point from './Point' // eslint-disable-line no-unused-vars
+import Rectangle from './Rectangle'
+
+/** A sequence of Rectangles. */
+export default class RectangleArray extends Array<Rectangle> {
+  constructor(...entries: Rectangle[]) {
+    super(...entries)
+
+    // Set the prototype explicitly.
+    // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    Reflect.setPrototypeOf(this, RectangleArray.prototype)
+  }
+
+  /** @arg {!Rectangle} rectangle
+      @return {!boolean} true if rectangle is an entry, false if absent. */
+  has(rectangle: Rectangle): boolean {
+    return this.some((entry: Rectangle): boolean => entry.equal(rectangle))
+  }
+
+  /** @arg {!Point} point
+      @return {!RectangleArray} Entries intersecting point. */
+  at(point: Point): RectangleArray {
+    return new RectangleArray(...this.filter((entry: Rectangle): boolean =>
+      entry.containsPoint(point)))
+  }
+
+  /** @arg {!Rectangle} rectangle
+      @return {!RectangleArray} Entries contained by rectangle. */
+  within(rectangle: Rectangle): RectangleArray {
+    return new RectangleArray(...this.filter((entry: Rectangle): boolean =>
+      rectangle.containsRectangle(entry)))
+  }
+
+  /** @arg {!Rectangle} rectangle
+      @return {!RectangleArray} Entries intersecting rectangle. */
+  intersecting(rectangle: Rectangle): RectangleArray {
+    return new RectangleArray(...this.filter((entry: Rectangle): boolean =>
+      entry.intersects(rectangle)))
+  }
+
+  /** @arg {!Rectangle} rectangle
+      @return {!RectangleArray} The relative complement of rectangle in all
+                                entries as represented by a union of nonempty,
+                                maximally sized, possibly overlapping
+                                Rectangles. */
+  complement(rectangle: Rectangle): RectangleArray {
+    return new RectangleArray(...Rectangle.filterNonemptySuperset(
+      this.reduce((array: Rectangle[], entry: Rectangle): Rectangle[] =>
+        array.concat(entry.complement(rectangle)), [])
+    ))
+  }
+
+  difference(array: RectangleArray): RectangleArray {
+    // eslint-disable-next-line no-shadow
+    const reduce = (array: RectangleArray, entry: Rectangle): RectangleArray =>
+      array.remove(entry)
+    return array.reduce(reduce, this)
+  }
+
+  remove(rectangle: Rectangle): RectangleArray {
+    const result: RectangleArray = new RectangleArray(...this)
+    const index: number = result.indexOf(rectangle)
+    if (index > -1) result.splice(index, 1)
+    return result
+  }
+
+  indexOf(rectangle: Rectangle): number {
+    return this.findIndex((entry: Rectangle) => entry.equal(rectangle))
+  }
+
+  // todo: consider order here. The client should decide whether order is
+  //       important in how they construct Rectangles and RectangleArrays. This
+  //       isn't a concern here.
+  /** @arg {?RectangleArray} array
+      @return {!boolean} true if both arrays have identical unordered sequences,
+                         false if entry memberships differ. */
+  equal(array?: RectangleArray): boolean {
+    return !!array
+      && this.length === array.length && !this.difference(array).length
+  }
+
+  toString() {
+    const reduce = (string: string, entry: Rectangle): string =>
+        `${string}${string ? ' ' : ''}${entry}`
+    return `[${this.reduce(reduce, '')}]`
+  }
+}
---
+++ test/RectangleArray.ts
@@ -0,0 +1,474 @@
+// eslint-disable-line max-lines
+import Point from '../src/Point'
+import Rectangle from '../src/Rectangle'
+import RectangleArray from '../src/RectangleArray'
+
+describe('RectangleArray', () => {
+  describe('[a|-1 -1 1 1|]', () => {
+    const a: Rectangle = new Rectangle(new Point(-1, -1), new Point(1, 1))
+    const subject: RectangleArray = new RectangleArray(a)
+
+    test('has |0 0 0 0|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(0, 0))
+      expect(subject.has(input)).toBe(false)
+    })
+
+    test('has |0 0 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(1, 1))
+      expect(subject.has(input)).toBe(false)
+    })
+
+    test('has |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.has(input)).toBe(false)
+    })
+
+    test('has a|-1 -1 1 1|', () => {
+      expect(subject.has(a)).toBe(true)
+    })
+
+    test('at (0 0)', () => {
+      expect(subject.at(new Point(0, 0)).equal(subject)).toBe(true)
+    })
+
+    test('at (1 1)', () => {
+      expect(subject.at(new Point(1, 1)).equal(subject)).toBe(true)
+    })
+
+    test('at (3 1)', () => {
+      expect(subject.at(new Point(3, 1)).equal(new RectangleArray())).toBe(true)
+    })
+
+    test('within |0 0 0 0|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(0, 0))
+      expect(subject.within(input).equal(new RectangleArray())).toBe(true)
+    })
+
+    test('within |0 0 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(1, 1))
+      expect(subject.within(input).equal(new RectangleArray())).toBe(true)
+    })
+
+    test('within |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.within(input).equal(new RectangleArray())).toBe(true)
+    })
+
+    test('within |-5 -5 5 5|', () => {
+      const input: Rectangle = new Rectangle(new Point(-5, -5), new Point(5, 5))
+      expect(subject.within(input).equal(subject)).toBe(true)
+    })
+
+    test('intersecting |0 0 0 0|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(0, 0))
+      expect(subject.intersecting(input).equal(subject)).toBe(true)
+    })
+
+    test('intersecting |0 0 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(1, 1))
+      expect(subject.intersecting(input).equal(subject)).toBe(true)
+    })
+
+    test('intersecting |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.intersecting(input).equal(subject)).toBe(true)
+    })
+
+    test('complement |0 0 0 0|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(0, 0))
+      const expected: RectangleArray = new RectangleArray(
+        new Rectangle(new Point(-1, 0), new Point(1, 1)),
+        new Rectangle(new Point(-1, -1), new Point(0, 1)),
+        new Rectangle(new Point(-1, -1), new Point(1, 0)),
+        new Rectangle(new Point(0, -1), new Point(1, 1))
+      )
+      expect(subject.complement(input).equal(expected)).toBe(true)
+    })
+
+    test('complement |0 0 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(1, 1))
+      const expected: RectangleArray = new RectangleArray(
+        new Rectangle(new Point(-1, -1), new Point(0, 1)),
+        new Rectangle(new Point(-1, -1), new Point(1, 0))
+      )
+      expect(subject.complement(input).equal(expected)).toBe(true)
+    })
+
+    test('complement |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      const expected: RectangleArray = new RectangleArray(
+        new Rectangle(new Point(-1, -1), new Point(0, 1)),
+        new Rectangle(new Point(-1, -1), new Point(1, 0))
+      )
+      expect(subject.complement(input).equal(expected)).toBe(true)
+    })
+
+    test('difference [|0 0 0 0|]', () => {
+      const input: RectangleArray = new RectangleArray(
+        new Rectangle(new Point(0, 0), new Point(0, 0))
+      )
+      expect(subject.difference(input).equal(subject)).toBe(true)
+    })
+
+    test('difference [|0 0 1 1|]', () => {
+      const input: RectangleArray = new RectangleArray(
+        new Rectangle(new Point(0, 0), new Point(1, 1))
+      )
+      expect(subject.difference(input).equal(subject)).toBe(true)
+    })
+
+    test('difference [|0 0 3 1|]', () => {
+      const input: RectangleArray = new RectangleArray(
+        new Rectangle(new Point(0, 0), new Point(3, 1))
+      )
+      expect(subject.difference(input).equal(subject)).toBe(true)
+    })
+
+    test('difference self', () => {
+      expect(subject.difference(subject).equal(new RectangleArray())).toBe(true)
+    })
+
+    test('remove |0 0 0 0|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(0, 0))
+      expect(subject.remove(input).equal(subject)).toBe(true)
+    })
+
+    test('remove |0 0 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(1, 1))
+      expect(subject.remove(input).equal(subject)).toBe(true)
+    })
+
+    test('remove |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.remove(input).equal(subject)).toBe(true)
+    })
+
+    test('remove |-1 -1 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(-1, -1), new Point(1, 1))
+      expect(subject.remove(input).equal(new RectangleArray())).toBe(true)
+    })
+
+    test('indexOf |0 0 0 0|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(0, 0))
+      expect(subject.indexOf(input)).toBe(-1)
+    })
+
+    test('indexOf |0 0 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(1, 1))
+      expect(subject.indexOf(input)).toBe(-1)
+    })
+
+    test('indexOf |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.indexOf(input)).toBe(-1)
+    })
+
+    test('indexOf |-1 -1 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(-1, -1), new Point(1, 1))
+      expect(subject.indexOf(input)).toBe(0)
+    })
+
+    test('equal self', () => expect(subject.equal(subject)).toBe(true))
+
+    test('equal undefined', () => expect(subject.equal()).toBe(false))
+
+    test('toString()', () => expect(subject.toString()).toBe('[|-1 -1 1 1|]'))
+  })
+
+  // 5◦◦◦◦◦◦
+  // 4◦◦◦ Df
+  // 3◦◦◦ ff
+  // 2◦◦◦cFf
+  // 1bb◦cCf
+  // 0Ab◦◦◦◦
+  //  012345
+  // eslint-disable-next-line max-len
+  describe('[a|0 0 0 0| b|0 0 1 1| c|3 1 4 1| d|4 4 4 4| e|4 4 4 4| f|4 1 4 4|]', () => {
+    const a: Rectangle = new Rectangle(new Point(0, 0), new Point(0, 0))
+    const b: Rectangle = new Rectangle(new Point(0, 0), new Point(1, 1))
+    const c: Rectangle = new Rectangle(new Point(3, 1), new Point(4, 2))
+    const d: Rectangle = new Rectangle(new Point(4, 4), new Point(4, 4))
+    const e: Rectangle = new Rectangle(new Point(4, 4), new Point(4, 4))
+    const f: Rectangle = new Rectangle(new Point(4, 1), new Point(5, 4))
+    const subject: RectangleArray = new RectangleArray(a, b, c, d, e, f)
+
+    test('has a|0 0 0 0|', () => {
+      expect(subject.has(a)).toBe(true)
+    })
+
+    test('has b|0 0 1 1|', () => {
+      expect(subject.has(b)).toBe(true)
+    })
+
+    test('has |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.has(input)).toBe(false)
+    })
+
+    test('at (0 0)', () => {
+      const expected: RectangleArray = new RectangleArray(a, b)
+      expect(subject.at(new Point(0, 0)).equal(expected)).toBe(true)
+    })
+
+    test('at (1 1)', () => {
+      const expected: RectangleArray = new RectangleArray(b)
+      expect(subject.at(new Point(1, 1)).equal(expected)).toBe(true)
+    })
+
+    test('at (3 1)', () => {
+      const expected: RectangleArray = new RectangleArray(c)
+      expect(subject.at(new Point(3, 1)).equal(expected)).toBe(true)
+    })
+
+    test('at (5 5)', () => {
+      expect(subject.at(new Point(5, 5)).equal(new RectangleArray())).toBe(true)
+    })
+
+    test('within a|0 0 0 0|', () => {
+      expect(subject.within(a).equal(new RectangleArray(a))).toBe(true)
+    })
+
+    test('within b|0 0 1 1|', () => {
+      const expected: RectangleArray = new RectangleArray(a, b)
+      expect(subject.within(b).equal(expected)).toBe(true)
+    })
+
+    test('within |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      const expected: RectangleArray = new RectangleArray(a, b)
+      expect(subject.within(input).equal(expected)).toBe(true)
+    })
+
+    test('within |5 5 5 5|', () => {
+      const input: Rectangle = new Rectangle(new Point(5, 5), new Point(5, 5))
+      expect(subject.within(input).equal(new RectangleArray())).toBe(true)
+    })
+
+    test('intersecting a|0 0 0 0|', () => {
+      expect(subject.intersecting(a).equal(new RectangleArray(a, b))).toBe(true)
+    })
+
+    test('intersecting b|0 0 1 1|', () => {
+      expect(subject.intersecting(b).equal(new RectangleArray(a, b))).toBe(true)
+    })
+
+    test('intersecting |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      const expected: RectangleArray = new RectangleArray(a, b, c)
+      expect(subject.intersecting(input).equal(expected)).toBe(true)
+    })
+
+    test('complement a|0 0 0 0|', () => {
+      const expected: RectangleArray = new RectangleArray(b, c, f)
+      expect(subject.complement(a).equal(expected)).toBe(true)
+    })
+
+    test('complement b|0 0 1 1|', () => {
+      expect(subject.complement(b).equal(new RectangleArray(c, f))).toBe(true)
+    })
+
+    test('complement |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      const expected: RectangleArray = new RectangleArray(c, f)
+      expect(subject.complement(input).equal(expected)).toBe(true)
+    })
+
+    test('difference [a|0 0 0 0|]', () => {
+      const input: RectangleArray = new RectangleArray(a)
+      const expected: RectangleArray = new RectangleArray(b, c, d, e, f)
+      expect(subject.difference(input).equal(expected)).toBe(true)
+    })
+
+    test('difference [b|0 0 1 1|]', () => {
+      const input: RectangleArray = new RectangleArray(b)
+      const expected: RectangleArray = new RectangleArray(a, c, d, e, f)
+      expect(subject.difference(input).equal(expected)).toBe(true)
+    })
+
+    test('difference [|0 0 3 1|]', () => {
+      const input: RectangleArray = new RectangleArray(
+        new Rectangle(new Point(0, 0), new Point(3, 1))
+      )
+      expect(subject.difference(input).equal(subject)).toBe(true)
+    })
+
+    test('difference self', () => {
+      expect(subject.difference(subject).equal(new RectangleArray())).toBe(true)
+    })
+
+    test('remove a|0 0 0 0|', () => {
+      const expected: RectangleArray = new RectangleArray(b, c, d, e, f)
+      expect(subject.remove(a).equal(expected)).toBe(true)
+    })
+
+    test('remove b|0 0 1 1|', () => {
+      const expected: RectangleArray = new RectangleArray(a, c, d, e, f)
+      expect(subject.remove(b).equal(expected)).toBe(true)
+    })
+
+    test('remove |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.remove(input).equal(subject)).toBe(true)
+    })
+
+    test('indexOf a|0 0 0 0|', () => {
+      expect(subject.indexOf(a)).toBe(0)
+    })
+
+    test('indexOf b|0 0 1 1|', () => {
+      expect(subject.indexOf(b)).toBe(1)
+    })
+
+    test('indexOf |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.indexOf(input)).toBe(-1)
+    })
+
+    test('equal self', () => expect(subject.equal(subject)).toBe(true))
+
+    test('equal undefined', () => expect(subject.equal()).toBe(false))
+
+    test('toString', () => {
+      // eslint-disable-next-line max-len
+      const expected: string = '[|0 0 0 0| |0 0 1 1| |3 1 4 2| |4 4 4 4| |4 4 4 4| |4 1 5 4|]'
+      expect(subject.toString()).toBe(expected)
+    })
+  })
+
+  describe('[]', () => {
+    const subject: RectangleArray = new RectangleArray()
+
+    test('has |0 0 0 0|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(0, 0))
+      expect(subject.has(input)).toBe(false)
+    })
+
+    test('has |0 0 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(1, 1))
+      expect(subject.has(input)).toBe(false)
+    })
+
+    test('has |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.has(input)).toBe(false)
+    })
+
+    test('at (0 0)', () => {
+      expect(subject.at(new Point(0, 0)).equal(subject)).toBe(true)
+    })
+
+    test('at (1 1)', () => {
+      expect(subject.at(new Point(1, 1)).equal(subject)).toBe(true)
+    })
+
+    test('at (3 1)', () => {
+      expect(subject.at(new Point(3, 1)).equal(subject)).toBe(true)
+    })
+
+    test('within |0 0 0 0|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(0, 0))
+      expect(subject.within(input).equal(subject)).toBe(true)
+    })
+
+    test('within |0 0 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(1, 1))
+      expect(subject.within(input).equal(subject)).toBe(true)
+    })
+
+    test('within |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.within(input).equal(subject)).toBe(true)
+    })
+
+    test('intersecting |0 0 0 0|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(0, 0))
+      expect(subject.intersecting(input).equal(subject)).toBe(true)
+    })
+
+    test('intersecting |0 0 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(1, 1))
+      expect(subject.intersecting(input).equal(subject)).toBe(true)
+    })
+
+    test('intersecting |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.intersecting(input).equal(subject)).toBe(true)
+    })
+
+    test('complement |0 0 0 0|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(0, 0))
+      expect(subject.complement(input).equal(subject)).toBe(true)
+    })
+
+    test('complement |0 0 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(1, 1))
+      expect(subject.complement(input).equal(subject)).toBe(true)
+    })
+
+    test('complement |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.complement(input).equal(subject)).toBe(true)
+    })
+
+    test('difference [|0 0 0 0|]', () => {
+      const input: RectangleArray = new RectangleArray(
+        new Rectangle(new Point(0, 0), new Point(0, 0))
+      )
+      expect(subject.difference(input).equal(subject)).toBe(true)
+    })
+
+    test('difference [|0 0 1 1|]', () => {
+      const input: RectangleArray = new RectangleArray(
+        new Rectangle(new Point(0, 0), new Point(1, 1))
+      )
+      expect(subject.difference(input).equal(subject)).toBe(true)
+    })
+
+    test('difference [|0 0 3 1|]', () => {
+      const input: RectangleArray = new RectangleArray(
+        new Rectangle(new Point(0, 0), new Point(3, 1))
+      )
+      expect(subject.difference(input).equal(subject)).toBe(true)
+    })
+
+    test('difference self', () => {
+      expect(subject.difference(subject).equal(subject)).toBe(true)
+    })
+
+    test('remove |0 0 0 0|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(0, 0))
+      expect(subject.remove(input).equal(subject)).toBe(true)
+    })
+
+    test('remove |0 0 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(1, 1))
+      expect(subject.remove(input).equal(subject)).toBe(true)
+    })
+
+    test('remove |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.remove(input).equal(subject)).toBe(true)
+    })
+
+    test('indexOf |0 0 0 0|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(0, 0))
+      expect(subject.indexOf(input)).toBe(-1)
+    })
+
+    test('indexOf |0 0 1 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(1, 1))
+      expect(subject.indexOf(input)).toBe(-1)
+    })
+
+    test('indexOf |0 0 3 1|', () => {
+      const input: Rectangle = new Rectangle(new Point(0, 0), new Point(3, 1))
+      expect(subject.indexOf(input)).toBe(-1)
+    })
+
+    test('equal self', () => expect(subject.equal(subject)).toBe(true))
+
+    test('equal undefined', () => expect(subject.equal()).toBe(false))
+
+    test('toString()', () => expect(subject.toString()).toBe('[]'))
+  })
+})
